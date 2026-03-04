da3e2376b671420ae6e3c8b1b666cfd5ea385c96
takzen <takzen.app@gmail.com>
2026-03-04 18:41:07 +0100

    fix: setup next.js API rewrites to prevent mixed content HTTP blocks

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/v1/:path*',
+        destination: `${process.env.BACKEND_URL || 'http://159.69.22.100:8000'}/:path*`, // Proxy to Backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
